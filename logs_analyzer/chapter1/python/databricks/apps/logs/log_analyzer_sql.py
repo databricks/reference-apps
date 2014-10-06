f501b02b20216d8dc3ac846c35f0bb336c086ec6
Vida Ha <vida@databricks.com>
2014-10-06 14:55:00 -0700

    Update Python to use Row instead of deprecated dictionaries

--- logs_analyzer/chapter1/python/databricks/apps/logs/log_analyzer_sql.py
+++ logs_analyzer/chapter1/python/databricks/apps/logs/log_analyzer_sql.py
@@ -1,7 +1,9 @@
 # To run this code:
 #
-# % spark-submit databricks/apps/logs/log_analyzer_sql.py
-#    --py-files databricks/apps/logs/apache_access_log.py
+# % spark-submit \
+#    --py-files databricks/apps/logs/apache_access_log.py \
+#    --master local[4] \
+#    databricks/apps/logs/log_analyzer_sql.py \
 #    ../../data/apache.accesslog
 from pyspark import SparkContext, SparkConf
 from pyspark.sql import SQLContext
@@ -9,7 +11,7 @@ from pyspark.sql import SQLContext
 import apache_access_log
 import sys
 
-conf = SparkConf().setAppName("Log Analyzer").setMaster("local[4]")
+conf = SparkConf().setAppName("Log Analyzer")
 sc = SparkContext(conf=conf)
 sqlContext = SQLContext(sc)
 
@@ -26,34 +28,34 @@ schema_access_logs.registerAsTable("logs")
 # Calculate statistics based on the content size.
 content_size_stats = (sqlContext
                       .sql("SELECT %s, %s, %s, %s FROM logs" % (
-                           "SUM(contentSize) as theSum",
+                           "SUM(content_size) as theSum",
                            "COUNT(*) as theCount",
-                           "MIN(contentSize) as theMin",
-                           "MAX(contentSize) as theMax"))
+                           "MIN(content_size) as theMin",
+                           "MAX(content_size) as theMax"))
                       .first())
 print "Content Size Avg: %i, Min: %i, Max: %s" % (
-    content_size_stats["theSum"] / content_size_stats["theCount"],
-    content_size_stats["theMin"],
-    content_size_stats["theMax"]
+    content_size_stats[0] / content_size_stats[1],
+    content_size_stats[2],
+    content_size_stats[3]
 )
 
 # Response Code to Count
 responseCodeToCount = (sqlContext
-                       .sql("SELECT responseCode, COUNT(*) AS theCount FROM logs GROUP BY responseCode LIMIT 100")
-                       .map(lambda row: (row['responseCode'], row['theCount']))
+                       .sql("SELECT response_code, COUNT(*) AS theCount FROM logs GROUP BY response_code LIMIT 100")
+                       .map(lambda row: (row[0], row[1]))
                        .collect())
 print "Response Code Counts: %s" % (responseCodeToCount)
 
 # Any IPAddress that has accessed the server more than 10 times.
 ipAddresses = (sqlContext
-               .sql("SELECT ipAddress, COUNT(*) AS total FROM logs GROUP BY ipAddress HAVING total > 10 LIMIT 100")
-               .map(lambda row: row['ipAddress'])
+               .sql("SELECT ip_address, COUNT(*) AS total FROM logs GROUP BY ip_address HAVING total > 10 LIMIT 100")
+               .map(lambda row: row[0])
                .collect())
 print "All IPAddresses > 10 times: %s" % ipAddresses
 
 # Top Endpoints
 topEndpoints = (sqlContext
                 .sql("SELECT endpoint, COUNT(*) AS total FROM logs GROUP BY endpoint ORDER BY total DESC LIMIT 10")
-                .map(lambda row: (row['endpoint'], row['total']))
+                .map(lambda row: (row[0], row[1]))
                 .collect())
 print "Top Endpoints: %s" % (topEndpoints)
