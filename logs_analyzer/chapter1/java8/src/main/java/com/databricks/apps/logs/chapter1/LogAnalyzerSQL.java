e0d0c8a0a7340950314ebd57a054750f26f631fc
tashoyan <tashoyan@gmail.com>
2016-10-18 15:53:52 +0300

    Update LogAnalyzerSQL with Spark 2 API

--- logs_analyzer/chapter1/java8/src/main/java/com/databricks/apps/logs/chapter1/LogAnalyzerSQL.java
+++ logs_analyzer/chapter1/java8/src/main/java/com/databricks/apps/logs/chapter1/LogAnalyzerSQL.java
@@ -1,17 +1,14 @@
 package com.databricks.apps.logs.chapter1;
 
 import com.databricks.apps.logs.ApacheAccessLog;
-
-import org.apache.spark.SparkConf;
+import java.util.List;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
-import org.apache.spark.sql.DataFrame;
-import org.apache.spark.sql.SQLContext;
+import org.apache.spark.sql.Dataset;
+import org.apache.spark.sql.Encoders;
 import org.apache.spark.sql.Row;
-
+import org.apache.spark.sql.SparkSession;
 import scala.Tuple2;
-
-import java.util.List;
 
 /**
  * LogAnalyzerSQL shows how to use SQL syntax with Spark.
@@ -27,9 +24,11 @@ public class LogAnalyzerSQL {
 
   public static void main(String[] args) {
     // Initialize the Spark context.
-    SparkConf conf = new SparkConf().setAppName("Log Analyzer SQL");
-    JavaSparkContext sc = new JavaSparkContext(conf);
-    SQLContext sqlContext = new SQLContext(sc);
+    SparkSession sparkSession = SparkSession
+            .builder()
+            .appName("Log Analyzer SQL")
+            .getOrCreate();
+    JavaSparkContext sc = new JavaSparkContext(sparkSession.sparkContext());
 
     if (args.length == 0) {
       System.out.println("Must specify an access logs file.");
@@ -39,45 +38,40 @@ public class LogAnalyzerSQL {
     JavaRDD<ApacheAccessLog> accessLogs = sc.textFile(logFile)
         .map(ApacheAccessLog::parseFromLogLine);
 
-    // Spark SQL can imply a schema for a table if given a Java class with getters and setters.
-    DataFrame sqlDataFrame =
-        sqlContext.createDataFrame(accessLogs, ApacheAccessLog.class);
-    sqlDataFrame.registerTempTable("logs");
-    sqlContext.cacheTable("logs");
+    // Create Spark DataFrame from the RDD.
+    Dataset<Row> accessLogsDf =
+            sparkSession.createDataFrame(accessLogs, ApacheAccessLog.class);
+    accessLogsDf.createOrReplaceTempView("logs");
 
     // Calculate statistics based on the content size.
-    Row contentSizeStats = sqlContext.sql(
-        "SELECT SUM(contentSize), COUNT(*), MIN(contentSize), MAX(contentSize) FROM logs")
-            .javaRDD()
-            .collect()
-            .get(0);
+    Row contentSizeStats = sparkSession
+            .sql("SELECT SUM(contentSize), COUNT(*), MIN(contentSize), MAX(contentSize) FROM logs")
+            .first();
     System.out.println(String.format("Content Size Avg: %s, Min: %s, Max: %s",
         contentSizeStats.getLong(0) / contentSizeStats.getLong(1),
         contentSizeStats.getLong(2),
         contentSizeStats.getLong(3)));
 
     // Compute Response Code to Count.
-    List<Tuple2<Integer, Long>> responseCodeToCount = sqlContext
+    List<Tuple2<Integer, Long>> responseCodeToCount = sparkSession
         .sql("SELECT responseCode, COUNT(*) FROM logs GROUP BY responseCode LIMIT 100")
-        .javaRDD()
-        .mapToPair(row -> new Tuple2<>(row.getInt(0), row.getLong(1)))
-        .collect();
+        .map(row -> new Tuple2<>(row.getInt(0), row.getLong(1)),
+                Encoders.tuple(Encoders.INT(), Encoders.LONG()))
+        .collectAsList();
     System.out.println(String.format("Response code counts: %s", responseCodeToCount));
 
     // Any IPAddress that has accessed the server more than 10 times.
-    List<String> ipAddresses = sqlContext
+    List<String> ipAddresses = sparkSession
         .sql("SELECT ipAddress, COUNT(*) AS total FROM logs GROUP BY ipAddress HAVING total > 10 LIMIT 100")
-        .javaRDD()
-        .map(row -> row.getString(0))
-        .collect();
+        .map(row -> row.getString(0), Encoders.STRING())
+        .collectAsList();
     System.out.println(String.format("IPAddresses > 10 times: %s", ipAddresses));
 
     // Top Endpoints.
-    List<Tuple2<String, Long>> topEndpoints = sqlContext
+    List<Tuple2<String, Long>> topEndpoints = sparkSession
         .sql("SELECT endpoint, COUNT(*) AS total FROM logs GROUP BY endpoint ORDER BY total DESC LIMIT 10")
-        .javaRDD()
-        .map(row -> new Tuple2<>(row.getString(0), row.getLong(1)))
-        .collect();
+        .map(row -> new Tuple2<>(row.getString(0), row.getLong(1)), Encoders.tuple(Encoders.STRING(), Encoders.LONG()))
+        .collectAsList();
     System.out.println(String.format("Top Endpoints: %s", topEndpoints));
 
     sc.stop();
