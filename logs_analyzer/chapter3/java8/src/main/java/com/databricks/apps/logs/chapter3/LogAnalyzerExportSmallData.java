4c4166323aca6654bc563b942e65e29ddc73672e
tashoyan <tashoyan@gmail.com>
2016-10-20 19:34:41 +0300

    Use try-with-resources to prevent file descriptor leak. Describe how to run the example.

--- logs_analyzer/chapter3/java8/src/main/java/com/databricks/apps/logs/chapter3/LogAnalyzerExportSmallData.java
+++ logs_analyzer/chapter3/java8/src/main/java/com/databricks/apps/logs/chapter3/LogAnalyzerExportSmallData.java
@@ -10,6 +10,16 @@ import org.apache.spark.sql.SparkSession;
 import scala.Tuple2;
 import scala.Tuple4;
 
+/**
+ * LogAnalyzerExportSmallData shows how to export data of small size to a file.
+ * 
+  * Example command to run:
+ * %  ${YOUR_SPARK_HOME}/bin/spark-submit
+ *     --class "com.databricks.apps.logs.chapter3.LogAnalyzerExportSmallData"
+ *     --master spark://YOUR_SPARK_MASTER
+ *     target/log-analyzer-2.0.jar
+ *     ../../data/apache.access.log output.log
+ */
 public class LogAnalyzerExportSmallData {
 
   public static void main(String[] args) throws IOException {
@@ -34,27 +44,25 @@ public class LogAnalyzerExportSmallData {
     LogStatistics logStatistics = logAnalyzerRDD.processRdd(accessLogs);
 
     String outputFile = args[1];
-    Writer out = new BufferedWriter(
-        new OutputStreamWriter(new FileOutputStream(outputFile)));
+    try (Writer out = new BufferedWriter(
+            new OutputStreamWriter(new FileOutputStream(outputFile)))) {
+        Tuple4<Long, Long, Long, Long> contentSizeStats =
+                logStatistics.getContentSizeStats();
+        out.write(String.format("Content Size Avg: %s, Min: %s, Max: %s\n",
+                contentSizeStats._1() / contentSizeStats._2(),
+                contentSizeStats._3(),
+                contentSizeStats._4()));
 
-    Tuple4<Long, Long, Long, Long> contentSizeStats =
-        logStatistics.getContentSizeStats();
-    out.write(String.format("Content Size Avg: %s, Min: %s, Max: %s\n",
-        contentSizeStats._1() / contentSizeStats._2(),
-        contentSizeStats._3(),
-        contentSizeStats._4()));
+        List<Tuple2<Integer, Long>> responseCodeToCount =
+                logStatistics.getResponseCodeToCount();
+        out.write(String.format("Response code counts: %s\n", responseCodeToCount));
 
-    List<Tuple2<Integer, Long>> responseCodeToCount =
-        logStatistics.getResponseCodeToCount();
-    out.write(String.format("Response code counts: %s\n", responseCodeToCount));
+        List<String> ipAddresses = logStatistics.getIpAddresses();
+        out.write(String.format("IPAddresses > 10 times: %s\n", ipAddresses));
 
-    List<String> ipAddresses = logStatistics.getIpAddresses();
-    out.write(String.format("IPAddresses > 10 times: %s\n", ipAddresses));
-
-    List<Tuple2<String, Long>> topEndpoints = logStatistics.getTopEndpoints();
-    out.write(String.format("Top Endpoints: %s\n", topEndpoints));
-
-    out.close();
+        List<Tuple2<String, Long>> topEndpoints = logStatistics.getTopEndpoints();
+        out.write(String.format("Top Endpoints: %s\n", topEndpoints));
+    }
 
     sparkSession.stop();
   }
