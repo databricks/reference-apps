e7ab985cae3fb79b4cc6eea70dfa27ab93d1d656
tashoyan <tashoyan@gmail.com>
2016-11-17 22:51:14 +0300

    Adjust indentation

--- logs_analyzer/chapter1/scala/src/main/scala/com/databricks/apps/logs/chapter1/LogAnalyzerStreamingSQL.scala
+++ logs_analyzer/chapter1/scala/src/main/scala/com/databricks/apps/logs/chapter1/LogAnalyzerStreamingSQL.scala
@@ -27,55 +27,55 @@ object LogAnalyzerStreamingSQL extends App {
   val WINDOW_LENGTH = Seconds(30)
   val SLIDE_INTERVAL = Seconds(10)
 
-    val spark = SparkSession.builder().appName("Log Analyzer Streaming in Scala").getOrCreate()
-    import spark.implicits._
-    val streamingContext = new StreamingContext(spark.sparkContext, SLIDE_INTERVAL)
+  val spark = SparkSession.builder().appName("Log Analyzer Streaming in Scala").getOrCreate()
+  import spark.implicits._
+  val streamingContext = new StreamingContext(spark.sparkContext, SLIDE_INTERVAL)
 
-    val logLinesDStream = streamingContext.socketTextStream("localhost", 9999)
+  val logLinesDStream = streamingContext.socketTextStream("localhost", 9999)
 
-    val accessLogsDStream = logLinesDStream.map(ApacheAccessLog.parseLogLine).cache()
+  val accessLogsDStream = logLinesDStream.map(ApacheAccessLog.parseLogLine).cache()
 
-    val windowDStream = accessLogsDStream.window(WINDOW_LENGTH, SLIDE_INTERVAL)
+  val windowDStream = accessLogsDStream.window(WINDOW_LENGTH, SLIDE_INTERVAL)
 
-    windowDStream.foreachRDD(accessLogs => {
-      if (accessLogs.count() == 0) {
-        println("No access logs received in this time interval")
-      } else {
-        accessLogs.toDF().createOrReplaceTempView("logs")
+  windowDStream.foreachRDD(accessLogs => {
+    if (accessLogs.count() == 0) {
+      println("No access logs received in this time interval")
+    } else {
+      accessLogs.toDF().createOrReplaceTempView("logs")
 
-        // Calculate statistics based on the content size.
-        val contentSizeStats = spark
-          .sql("SELECT SUM(contentSize), COUNT(*), MIN(contentSize), MAX(contentSize) FROM logs")
-          .first()
-        println("Content Size Avg: %s, Min: %s, Max: %s".format(
-          contentSizeStats.getLong(0) / contentSizeStats.getLong(1),
-          contentSizeStats(2),
-          contentSizeStats(3)))
+      // Calculate statistics based on the content size.
+      val contentSizeStats = spark
+        .sql("SELECT SUM(contentSize), COUNT(*), MIN(contentSize), MAX(contentSize) FROM logs")
+        .first()
+      println("Content Size Avg: %s, Min: %s, Max: %s".format(
+        contentSizeStats.getLong(0) / contentSizeStats.getLong(1),
+        contentSizeStats(2),
+        contentSizeStats(3)))
 
-        // Compute Response Code to Count.
-        val responseCodeToCount = spark
-          .sql("SELECT responseCode, COUNT(*) FROM logs GROUP BY responseCode")
-          .map(row => (row.getInt(0), row.getLong(1)))
-          .take(1000)
-        println(s"""Response code counts: ${responseCodeToCount.mkString("[", ",", "]")}""")
+      // Compute Response Code to Count.
+      val responseCodeToCount = spark
+        .sql("SELECT responseCode, COUNT(*) FROM logs GROUP BY responseCode")
+        .map(row => (row.getInt(0), row.getLong(1)))
+        .take(1000)
+      println(s"""Response code counts: ${responseCodeToCount.mkString("[", ",", "]")}""")
 
-        // Any IPAddress that has accessed the server more than 10 times.
-        val ipAddresses = spark
-          .sql("SELECT ipAddress, COUNT(*) AS total FROM logs GROUP BY ipAddress HAVING total > 10")
-          .map(row => row.getString(0))
-          .take(100)
-        println(s"""IPAddresses > 10 times: ${ipAddresses.mkString("[", ",", "]")}""")
+      // Any IPAddress that has accessed the server more than 10 times.
+      val ipAddresses = spark
+        .sql("SELECT ipAddress, COUNT(*) AS total FROM logs GROUP BY ipAddress HAVING total > 10")
+        .map(row => row.getString(0))
+        .take(100)
+      println(s"""IPAddresses > 10 times: ${ipAddresses.mkString("[", ",", "]")}""")
 
-        // Top Endpoints.
-        val topEndpoints = spark
-          .sql("SELECT endpoint, COUNT(*) AS total FROM logs GROUP BY endpoint ORDER BY total DESC LIMIT 10")
-          .map(row => (row.getString(0), row.getLong(1)))
-          .collect()
-        println(s"""Top Endpoints: ${topEndpoints.mkString("[", ",", "]")}""")
-      }
-    })
+      // Top Endpoints.
+      val topEndpoints = spark
+        .sql("SELECT endpoint, COUNT(*) AS total FROM logs GROUP BY endpoint ORDER BY total DESC LIMIT 10")
+        .map(row => (row.getString(0), row.getLong(1)))
+        .collect()
+      println(s"""Top Endpoints: ${topEndpoints.mkString("[", ",", "]")}""")
+    }
+  })
 
-    // Start the streaming server.
-    streamingContext.start() // Start the computation
-    streamingContext.awaitTermination() // Wait for the computation to terminate
+  // Start the streaming server.
+  streamingContext.start() // Start the computation
+  streamingContext.awaitTermination() // Wait for the computation to terminate
 }
