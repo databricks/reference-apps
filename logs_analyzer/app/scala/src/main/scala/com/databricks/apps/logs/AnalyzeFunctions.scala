2d5f898390b1659dcf2a4434116f14ef8d1d03d6
tashoyan <tashoyan@gmail.com>
2016-11-23 20:02:00 +0300

    Code style guide: type annotations for public functions

--- logs_analyzer/app/scala/src/main/scala/com/databricks/apps/logs/AnalyzeFunctions.scala
+++ logs_analyzer/app/scala/src/main/scala/com/databricks/apps/logs/AnalyzeFunctions.scala
@@ -7,9 +7,11 @@ import org.apache.spark.rdd.RDD
   */
 trait AnalyzeFunctions {
 
-  def computeRunningSum = (nums: Seq[Long], current: Option[Long]) => Some(current.getOrElse(0L) + nums.sum)
+  def computeRunningSum: (Seq[Long], Option[Long]) => Option[Long]
+  = (nums: Seq[Long], current: Option[Long]) => Some(current.getOrElse(0L) + nums.sum)
 
-  def contentSizeStats = (accessLogs: RDD[ApacheAccessLog]) => {
+  def contentSizeStats: RDD[ApacheAccessLog] => Option[(Long, Long, Long, Long)]
+  = (accessLogs: RDD[ApacheAccessLog]) => {
     val contentSizes = accessLogs.map(_.contentSize).cache()
     if (contentSizes.count() > 0)
       Some((contentSizes.count(), contentSizes.reduce(_ + _), contentSizes.min(), contentSizes.max()))
@@ -17,11 +19,15 @@ trait AnalyzeFunctions {
       None
   }
 
-  def responseCodeCount = (accessLogs: RDD[ApacheAccessLog]) => accessLogs.map(_.responseCode -> 1L).reduceByKey(_ + _)
+  def responseCodeCount: RDD[ApacheAccessLog] => RDD[(Int, Long)]
+  = (accessLogs: RDD[ApacheAccessLog]) => accessLogs.map(_.responseCode -> 1L).reduceByKey(_ + _)
 
-  def ipAddressCount = (accessLogs: RDD[ApacheAccessLog]) => accessLogs.map(_.ipAddress -> 1L).reduceByKey(_ + _)
+  def ipAddressCount: RDD[ApacheAccessLog] => RDD[(String, Long)]
+  = (accessLogs: RDD[ApacheAccessLog]) => accessLogs.map(_.ipAddress -> 1L).reduceByKey(_ + _)
 
-  def filterIPAddress = (ipAddressCount: RDD[(String, Long)]) => ipAddressCount.filter(_._2 > 10).map(_._1)
+  def filterIPAddress: RDD[(String, Long)] => RDD[String]
+  = (ipAddressCount: RDD[(String, Long)]) => ipAddressCount.filter(_._2 > 10).map(_._1)
 
-  def endpointCount = (accessLogs: RDD[ApacheAccessLog]) => accessLogs.map(_.endpoint -> 1L).reduceByKey(_ + _)
+  def endpointCount: RDD[ApacheAccessLog] => RDD[(String, Long)]
+  = (accessLogs: RDD[ApacheAccessLog]) => accessLogs.map(_.endpoint -> 1L).reduceByKey(_ + _)
 }
